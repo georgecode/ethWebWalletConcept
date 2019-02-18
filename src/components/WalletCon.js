e3df2fad178e9e8646e1258d03f26825a71fbebb
georgecode <georgeczurlanis@gmail.com>
2019-02-18 02:36:03 -0800

    Clean up boiler Plate, Add prettier.js, add WalletCon

---
+++ src/components/WalletCon.js
@@ -0,0 +1,18 @@
+import React, { Component } from "react";
+
+const styles = {
+	test: {
+		color: "red"
+	}
+};
+
+class WalletCon extends Component {
+	//constructor(props) {
+	//	super(props);
+	//}
+
+	render() {
+		return <h1 style={styles.test}>WalletCon</h1>;
+	}
+}
+export default WalletCon;
